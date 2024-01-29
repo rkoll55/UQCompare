ce74c447876bdfe39f5f8fecc0c46c265ac6adf7
rkoll55 <rohankollambalath@gmail.com>
2024-01-29 19:11:27 +1000

    added footer and extra styling to courses section

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,18 +1,5 @@
 import { useState } from "react";
-
-const styles = {
-  search: {
-    fontSize: "1rem",
-    padding: "10px",
-    borderRadius: "15px",
-    border: "2px solid #5B247C",
-    width: "65%",
-    height: "2rem",
-    marginLeft: "auto",
-    marginRight: "auto",
-    marginTop: "2rem",
-  },
-};
+import Styles from "./styles/courses.module.css";
 
 function Search() {
   const [value, setValue] = useState("");
@@ -27,7 +14,7 @@ function Search() {
       value={value}
       onChange={handleChange}
       placeholder="Search for a Course..."
-      style={styles.search}
+      className={Styles.search}
     />
   );
 }
