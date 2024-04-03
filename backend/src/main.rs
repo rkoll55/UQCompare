7e297ebb2320bfc30a31e9ae0767f1926cf00c68
sainty0 <saintyrod@gmail.com>
2024-04-03 17:02:01 +1000

    Add cors for local enviroment testing

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,7 +2,7 @@ mod api;
 mod model;
 mod repository;
 
-use actix_web::{middleware::Logger, web::Data, App, HttpServer};
+use actix_web::{http, middleware::Logger, web::Data, App, HttpServer};
 use api::router::routes;
 use repository::ddb::DDBRepository;
 use actix_cors::Cors;
@@ -14,14 +14,23 @@ async fn main() -> std::io::Result<()> {
     env_logger::init();
 
     let config = aws_config::load_from_env().await;
+
     HttpServer::new(move || {
         let ddb_repo: DDBRepository =
             DDBRepository::init(String::from("uqcompare_courses"), config.clone());
         let ddb_data = Data::new(ddb_repo);
         let logger = Logger::default();
+
+        let cors = Cors::default()
+            .allowed_origin("http://localhost:8080")
+            .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
+            .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
+            .allowed_header(http::header::CONTENT_TYPE)
+            .max_age(3600);
+
         App::new()
             .wrap(logger)
-            .wrap(Cors::permissive())
+            .wrap(cors)
             .app_data(ddb_data.clone())
             .configure(routes)
     })
